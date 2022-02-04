2d8a115da9aec62d1fd21608cf64d8197d10cf54
DidinSalahudin <didinsalahudin@gmail.com>
2022-02-04 16:01:42 +0700

    Add Home

--- lib/core/config/router.dart
+++ lib/core/config/router.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:new_template/features/home/presentation/pages/home_page.dart';
 
 import '../../features/user/presentation/pages/user_add_page.dart';
 import '../../features/user/presentation/pages/user_page.dart';
 
-class RouterApp {
-  static const String userPageRoute = '/';
+class RouterConst {
+  static const String homePageRoute = '/';
+  static const String userPageRoute = '/user';
   static const String userAddPageRoute = '/add_user';
+}
 
+class RouterApp {
   static Route<dynamic>? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case userPageRoute:
+      case RouterConst.homePageRoute:
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      case RouterConst.userPageRoute:
         return MaterialPageRoute(builder: (_) => const UserPage());
-      case userAddPageRoute:
+      case RouterConst.userAddPageRoute:
         return MaterialPageRoute(builder: (_) => const UserAddPage());
       default:
         return MaterialPageRoute(builder: (_) => const UserPage());
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "New Template",
       onGenerateRoute: RouterApp.generateRoute,
-      initialRoute: RouterApp.userPageRoute,
+      initialRoute: RouterConst.homePageRoute,
     );
   }
 }
